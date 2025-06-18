6fa220193227ee954df82a40c2c68581108d32d6
garyscavone <garyscavone@users.noreply.github.com>
2025-06-18 09:08:39 -0400

    Updated radiation.m for thickpipe approximation.

--- radiation.m
+++ radiation.m
@@ -1,16 +1,19 @@
-function [Zr, R] = radiation( a, f, T, type )
+function [Zr, R] = radiation( a, f, T, type, b )
 % [ZR, R] = RADIATION( A, F, TYPE, T ) computes the radiation impedance ZR
 %      (not normalized by Zc) and reflectance (or reflection coefficient) R
-%      at frequencies specified in the 1D vector F (Hertz) and for A =
-%      output radius in meters. T is an optional air temperature in degrees
-%      Celsius (default = 20 C). TYPE is an optional parameter specifying a
-%      particular condition or formula. The default ('dalmont') is an
-%      unflanged approximation provided in [1]. Other options include the
-%      unflanged ('unflanged') solution by Levine & Schwinger (1948), an
-%      unflanged approximation by Causse ('causse') and the flanged
-%      ('flanged') solution of Norris and Sheng (1989).
+%      of a cylindrical pipe at frequencies specified in the 1D vector F
+%      (Hertz) and for pipe radius A (in meters). T is an optional air
+%      temperature in degrees Celsius (default = 20 C). TYPE is an optional
+%      parameter specifying a particular condition or formula. The default
+%      ('dalmont') is an unflanged approximation provided in [1]. Other
+%      options include the unflanged ('unflanged') solution by Levine &
+%      Schwinger (1948), an unflanged approximation by Causse ('causse'),
+%      the flanged ('flanged') solution of Norris and Sheng (1989) and a
+%      thick pipe ('thickpipe') approximation provided in [1]. The
+%      parameter B (in meters), which specifies the outer wall thickness,
+%      is required with the 'thickpipe' type.
 %
-% by Gary P. Scavone, McGill University, 2013-2024.
+% by Gary P. Scavone, McGill University, 2013-2025.
 % Based in part on functions from WIAT by Antoine Lefebvre.
 %
 % References:
@@ -39,7 +42,7 @@ function [Zr, R] = radiation( a, f, T, type )
 % Zr is:
 %     Zr = 0.25*ka^2 + 0.61j*ka
 
-if nargin < 2 || nargin > 4
+if nargin < 2 || nargin > 5
   error( 'radiation: Invalid number of arguments.');
 end
 if ~isvector(f)
@@ -50,6 +53,9 @@ if ~exist( 'T', 'var')
 end
 if ~exist( 'type', 'var')
   type = 'dalmont';
+end
+if strcmp( type, 'thickpipe' ) && nargin < 5
+  error( 'radiation: b is a required parameter for thickpipe type.' );
 end
 
 [c, rho] = thermoConstants( T );
@@ -95,8 +101,7 @@ elseif strcmp( type, 'dalmont' )
   
   % Unflanged pipe radiation impedance approximation (ka < 1.5) from
   % reference [1].
-  R0 = (1 + 0.2*ka - 0.084*ka2) ./ (1 + 0.2*ka + (0.5-0.084)*ka2); % ka<3.5
-  delta = 0.6133*((1 + 0.044*ka2)./(1 + 0.19*ka2) - 0.02*sin(2*ka).^2);
+  [R0, delta] = unflanged(ka, ka2);
   R = -abs(R0).*exp(-2*1i*ka.*delta);
   Zr = (1 + R) ./ (1 - R);
 
@@ -106,6 +111,7 @@ elseif strcmp( type, 'causse' )
   % reference [2], formula taken from reference [1].
   Zr = 1j*0.6113*ka - 1j*ka.^3 .* (0.036-0.034*log(ka) + 0.0187*ka2) + ...
     0.25*ka2 + ka.^4.*(0.0127+0.082*log(ka) - 0.023*ka2);
+  R = (Zr -1) ./ (Zr + 1);
   
 elseif strcmp( type, 'flanged' )
 
@@ -115,9 +121,40 @@ elseif strcmp( type, 'flanged' )
   % A.N. Norris and I.C. Sheng. "Acoustic radiation from a circular pipe
   % with an infinite flange." Journal of Sound and Vibration, Vol. 135,
   % pp. 85-93, 1989.
-  R0 = (1 + 0.323*ka - 0.077*ka2) ./ (1 + 0.323*ka + (1-0.077)*ka2);
-  delta = 0.8216*(1 + (0.77*ka).^2./(1 + 0.77*ka)).^(-1);
+  [R0, delta] = flanged(ka, ka2);
   R = -abs(R0).*exp(-2*1i*ka.*delta);
+  Zr = (1 + R) ./ (1 - R);
+
+elseif strcmp( type, 'thickpipe' )
+  
+  kb = 2 * pi * f * b / c;
+  [R0_uf, delta_uf] = unflanged(ka, ka2);
+  [R0_f, delta_f] = flanged(ka, ka2);
+  
+  % [1] Combine radiation length corrections Eq. (41) Due to phase wrapping
+  % issues, compute delta_c and R_c separately Use Eq. (41) in [1],
+  % together with frequency-dependent length corrections for the infinite
+  % flanged and unflanged pipes to get real valued thick pipe (or circular
+  % flanged) length correction.
+  delta_c = delta_f + (a/b) * ( delta_uf - delta_f) ...
+    + 0.057 * (a/b) * ( 1 - ( a / b)^5 );
+
+  % Real circular flange reflection coefficient as a weighted interpolation
+  % between infinite flanged and unflanged coefficients.
+  R0_c = R0_f.^((b - a)/b).*R0_uf.^(a/b);
+
+  % Complex flange reflectance, but without edge effect
+  R_flange = -abs(R0_c) .* exp( -2j * ka.* delta_c);
+
+  % Eq. 42b from [1], magnitude of "edge" reflection coefficient
+  R0_edge = -0.43 * ( ( b - a ) * a )/( b^2 ) * sin( kb / ( 1.85 - a/b ) ).^2;
+
+  % Complex edge reflection coefficient
+  R_edge = R0_edge .* exp( -1j*kb .* (1 + a/b * (2.3 - a/b - 0.3*(ka2))));
+
+  % Eq. 42a from [1], combined reflectance from interpolated flange plus
+  % edge effect
+  R = R_flange + R_edge;
   Zr = (1 + R) ./ (1 - R);
 
 else
@@ -125,3 +162,17 @@ else
 end
 
 Zr = Zr * rho * c / (pi * a.^2); % scale by Zc
+
+end
+
+% Dalmont unflanged approximation from [1]
+function [R0, delta] = unflanged(ka, ka2)
+  R0 = (1 + 0.2*ka - 0.084*ka2) ./ (1 + 0.2*ka + (0.5-0.084)*ka2); % ka<3.5
+  delta = 0.6133*((1 + 0.044*ka2)./(1 + 0.19*ka2) - 0.02*sin(2*ka).^2);
+end
+
+% Norris and Sheng flanged approximation from [4]
+function [R0, delta] = flanged(ka, ka2)
+  R0 = (1 + 0.323*ka - 0.077*ka2) ./ (1 + 0.323*ka + (1-0.077)*ka2);
+  delta = 0.8216*(1 + (0.77*ka).^2./(1 + 0.77*ka)).^(-1);
+end
